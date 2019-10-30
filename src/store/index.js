e74c58ea04e805facbdc750eb311e4d4d5451171
Leonardo Scapinello <leonardo.scapinello@outlook.com>
2019-10-30 20:26:28 -0300

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import Vuex from "vuex";
+import Vue from "vue";
+import largeSidebar from "./modules/largeSidebar";
+import compactSidebar from "./modules/compactSidebar";
+import chat from "./modules/chat";
+import config from "./modules/config";
+import authData from "./modules/authData";
+import invoice from "./modules/invoice";
+import cart from "./modules/cart";
+
+
+// Load Vuex
+Vue.use(Vuex);
+
+// Create store
+export default new Vuex.Store({
+  modules: {
+    largeSidebar,
+    compactSidebar,
+    chat,
+    config,
+    authData,
+    invoice,
+    cart,
+    
+  }
+});
